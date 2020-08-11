50983ed87c23f691c4655fb8813ff07190bcbf2f
CyberUnit <cyberunit@gmail.com>
2020-08-11 13:49:33 +0300

    fragment and adding it to the main activity, data source classes

--- app/src/main/java/ru/istislav/fragmentMyCourse/MainActivity.java
+++ app/src/main/java/ru/istislav/fragmentMyCourse/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.istislav.fragmentMyCourse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import ru.istislav.fragmentMyCourse.data.MyFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.myContainer);
+        if (fragment == null) {
+            fragment = new MyFragment();
+            fm.beginTransaction().add(R.id.myContainer, fragment).commit();
+        }
     }
 }
