3b66bbfbd08c5c839ef9abc1bdee11a196fe7708
CyberUnit <cyberunit@gmail.com>
2020-08-11 14:21:48 +0300

    course Array adapter, course data, list fragment, showing course list

--- app/src/main/java/ru/istislav/fragmentMyCourse/data/MyFragment.java
+++ app/src/main/java/ru/istislav/fragmentMyCourse/data/MyFragment.java
@@ -8,13 +8,26 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import java.util.List;
 
 import ru.istislav.fragmentMyCourse.R;
 
-public class MyFragment extends Fragment {
+public class MyFragment extends ListFragment {
+    List<Course> courses = new CourseData().courseList();
 
     public MyFragment() {
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CourseArrayAdapter adapter = new CourseArrayAdapter(getActivity(), R.layout.course_listitem, courses);
+
+        setListAdapter(adapter);
     }
 
     @Nullable
