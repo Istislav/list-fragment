15da85e13a95d4b8c8cc396c9b830e3d4aebc878
CyberUnit <cyberunit@gmail.com>
2020-08-11 18:35:13 +0300

    checking layout and using fragment in the current layout or opening new activity with same fragment inside.

--- app/src/main/java/ru/istislav/fragmentMyCourse/MainActivity.java
+++ app/src/main/java/ru/istislav/fragmentMyCourse/MainActivity.java
@@ -13,28 +13,37 @@ import ru.istislav.fragmentMyCourse.data.CourseListFragment;
 
 public class MainActivity extends AppCompatActivity implements CourseListFragment.Callbacks {
 
+    private boolean isTwoPage = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FragmentManager fm = getSupportFragmentManager();
-//        Fragment fragment = fm.findFragmentById(R.id.myContainer);
-//        if (fragment == null) {
-//            fragment = new CourseListFragment();
-//            fm.beginTransaction().add(R.id.myContainer, fragment).commit();
-//        }
+        if (findViewById(R.id.detailContainer) != null) {
+            isTwoPage = true;
+        }
+
 
 
     }
 
     @Override
     public void onItemSelected(Course course, int position) {
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();;
+        if (isTwoPage) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("course_id", position);
 
-        Intent intent = new Intent(MainActivity.this, CourseDetailActivity.class);
-        intent.putExtra("course_id", position);
-        startActivity(intent);
+            FragmentManager fm = getSupportFragmentManager();
+            CourseDetailFragment courseDetailFragment = new CourseDetailFragment();
+            courseDetailFragment.setArguments(bundle);
+            // Attention! You need to use replace, not add!
+            fm.beginTransaction().replace(R.id.detailContainer, courseDetailFragment).commit();
+        } else {
+            Intent intent = new Intent(MainActivity.this, CourseDetailActivity.class);
+            intent.putExtra("course_id", position);
+            startActivity(intent);
+        }
     }
 
 }
